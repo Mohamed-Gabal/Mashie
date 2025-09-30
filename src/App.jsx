944eef5200b951394ba01b9fe364cc8922c49c7d
Mohamed Gabal <abogabal672@gmail.com>
2025-09-30 12:00:10 +0300

    edit all categories

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,7 @@ import MainLayout from "./Layouts/MainLayout";
 
 // Pages
 import Home from "./Pages/Home/Home";
-import Cars from "./Pages/Cars/Cars";
-import Jobs from './Pages/Jobs/Jobs';
-import Electronics from './Pages/Electronics/Electronics';
-import Antiques from './Pages/Antiques/Antiques';
-import Fashion from './Pages/Fashion/Fashion';
 import FileServices from './Pages/FileServices/FileServices';
-import Food from './Pages/Food/Food';
-import Furniture from './Pages/Furniture/Furniture';
-import Gardens from './Pages/Gardens/Gardens';
-import Pets from './Pages/Pets/Pets';
-import RealEstate from './Pages/RealEstate/RealEstate';
-import Trips from './Pages/Trips/Trips';
 import AboutUs from "./Pages/AboutUs/AboutUs";
 import Blog from "./Pages/Blog/Blog";
 import ContactUs from "./Pages/ContactUs/ContactUs";
@@ -41,18 +30,7 @@ const App = () => {
     <Routes>
       {/* صفحات عامة داخل MainLayout */}
       <Route path="/" element={<MainLayout><Home /></MainLayout>} />
-      <Route path="/cars" element={<MainLayout><Cars /></MainLayout>} />
-      <Route path="/jobs" element={<MainLayout><Jobs /></MainLayout>} />
-      <Route path="/electronics" element={<MainLayout><Electronics /></MainLayout>} />
-      <Route path="/antiques" element={<MainLayout><Antiques /></MainLayout>} />
-      <Route path="/fashion" element={<MainLayout><Fashion /></MainLayout>} />
       <Route path="/fileServices" element={<MainLayout><FileServices /></MainLayout>} />
-      <Route path="/food" element={<MainLayout><Food /></MainLayout>} />
-      <Route path="/furniture" element={<MainLayout><Furniture /></MainLayout>} />
-      <Route path="/gardens" element={<MainLayout><Gardens /></MainLayout>} />
-      <Route path="/pets" element={<MainLayout><Pets /></MainLayout>} />
-      <Route path="/realEstate" element={<MainLayout><RealEstate /></MainLayout>} />
-      <Route path="/trips" element={<MainLayout><Trips /></MainLayout>} />
       <Route path="/aboutUs" element={<MainLayout><AboutUs /></MainLayout>} />
       <Route path="/blog" element={<MainLayout><Blog /></MainLayout>} />
       <Route path="/contactUs" element={<MainLayout><ContactUs /></MainLayout>} />
--- src/Services/categoreyService.jsx
+++ src/Services/categoreyService.jsx
@@ -1,8 +1,14 @@
 import { useEffect, useState } from "react";
 
-const useCategoryAds = (category) => {
+  const useCategoryAds = (category) => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  // function for edit time
+    const formTime = (formTimeApi) => {
+    const dataApi = new Date(formTimeApi);
+    return dataApi.toLocaleString();
+  };
 
   useEffect(() => {
     const fetchAds = async () => {
@@ -12,22 +18,24 @@ const useCategoryAds = (category) => {
         );
         const json = await res.json();
 
+        // show the data
         console.log("API Response:", json);
 
         // تأكد من وجود الـ array الصحيح
-        const adsArray = Array.isArray(json.data?.data?.ads) ? json.data.data.ads : [];
+        const adsArray = Array.isArray(json.data?.data?.ads)
+          ? json.data.data.ads
+          : [];
 
         const mapped = adsArray.map((item) => ({
+          // Data of Api
           id: item.id_ads,
           image: `https://api.mashy.sand.alrmoz.com/storage/${item.images[0]}`,
-          userImage: item.user?.image || "صوره مجهوله",
+          userImage: item.user?.image || "/images/logo.svg",
           userName: item.seller?.name || "مستخدم مجهول",
-          title: item.title,
+          title: item.information.description,
           desc: item.desc || item.description,
-          location: item.information?.location
-            ? `${item.information.location.city}, ${item.information.location.area}, ${item.information.location.address}`
-            : "غير محدد",
-          time: item.created_at || item.time || item.postedAt,
+          location: item.location.address,
+          time: formTime(item.created_at),
         }));
 
         setData(mapped);
@@ -37,7 +45,6 @@ const useCategoryAds = (category) => {
         setLoading(false);
       }
     };
-
     fetchAds();
   }, [category]);
 
