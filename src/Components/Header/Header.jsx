d56b0dc69d940a66fb93f984fa898c6fd97d8134
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-10-01 11:11:25 +0200

    add svg icon

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -60,7 +60,7 @@ const Header = () => {
             (<div className="">
               <Link type="button" onClick={() => setToggleProfileCard(!toggleProfileCard)} className="btn_profile">
                 <span>حسابي</span>
-                <img src="./Icons/CaretDownWhite.svg" alt="CaretDownWhite" />
+                <svg xmlns="http://www.w3.org/2000/svg" width={22} height={22} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-down-icon lucide-chevron-down"><path d="m6 9 6 6 6-6" /></svg>
               </Link>
 
               <div className="profile-card" style={{ height: toggleProfileCard ? "280px" : "0" }}>
@@ -73,7 +73,7 @@ const Header = () => {
                 </div>
                 <button className="profile-btn">عرض الملف الشخصي</button>
                 <div className="settings">
-                  <img src="./Icons/sitting.svg" alt="SignOut" className="gear" />
+                  <svg xmlns="http://www.w3.org/2000/svg" width={22} height={22} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-settings-icon lucide-settings"><path d="M9.671 4.136a2.34 2.34 0 0 1 4.659 0 2.34 2.34 0 0 0 3.319 1.915 2.34 2.34 0 0 1 2.33 4.033 2.34 2.34 0 0 0 0 3.831 2.34 2.34 0 0 1-2.33 4.033 2.34 2.34 0 0 0-3.319 1.915 2.34 2.34 0 0 1-4.659 0 2.34 2.34 0 0 0-3.32-1.915 2.34 2.34 0 0 1-2.33-4.033 2.34 2.34 0 0 0 0-3.831A2.34 2.34 0 0 1 6.35 6.051a2.34 2.34 0 0 0 3.319-1.915" /><circle cx={12} cy={12} r={3} /></svg>
                   <span>إعدادات الحساب</span>
                 </div>
                 <button className="logout-btn" onClick={() => removeCookie("token")}>
@@ -88,7 +88,8 @@ const Header = () => {
             </NavLink>
           }
           <NavLink to="/Advertisements" className="btn-add">
-            <GoPlus /> <span>اضف عرضك</span>
+            <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-plus-icon lucide-plus"><path d="M5 12h14" /><path d="M12 5v14" /></svg>
+            <span>اضف عرضك</span>
           </NavLink>
         </div>
       </div>
