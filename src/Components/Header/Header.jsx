aebc28042af5cc76b1b6d5bc06502f3ebf1a416b
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-10-02 13:59:52 +0200

    add profile-card to .mobile-login

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -31,11 +31,40 @@ const Header = () => {
         <div className="mobile-search">
           <input type="search" placeholder="ابحث هنا..." />
         </div>
+
         <div className="mobile-login">
-          <NavLink to="/login" onClick={closeMenu}>
-            <CiUser className="icons" />
-          </NavLink>
+          {cookies?.token?.data?.token && cookies?.token?.data?.token !== "undefined" ?
+            (<div className="">
+              <Link type="button" onClick={() => setToggleProfileCard(!toggleProfileCard)} className="profile_img">
+                <img src={userData.image} alt={userData?.name?.split(" ").map(word => word[0]).join("").toUpperCase()} className="user_img" />
+              </Link>
+
+              <div className="profile-card" style={{ height: toggleProfileCard ? "280px" : "0" }}>
+                <div className="user-info">
+                  <img src={userData.image} alt={userData?.name?.split(" ").map(word => word[0]).join("").toUpperCase()} className="user_img" />
+                  <div>
+                    <p className="greeting">أهلا</p>
+                    <p className="username">{userData?.name}</p>
+                  </div>
+                </div>
+                <button className="profile-btn">عرض الملف الشخصي</button>
+                <div className="settings">
+                  <svg xmlns="http://www.w3.org/2000/svg" width={22} height={22} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-settings-icon lucide-settings"><path d="M9.671 4.136a2.34 2.34 0 0 1 4.659 0 2.34 2.34 0 0 0 3.319 1.915 2.34 2.34 0 0 1 2.33 4.033 2.34 2.34 0 0 0 0 3.831 2.34 2.34 0 0 1-2.33 4.033 2.34 2.34 0 0 0-3.319 1.915 2.34 2.34 0 0 1-4.659 0 2.34 2.34 0 0 0-3.32-1.915 2.34 2.34 0 0 1-2.33-4.033 2.34 2.34 0 0 0 0-3.831A2.34 2.34 0 0 1 6.35 6.051a2.34 2.34 0 0 0 3.319-1.915" /><circle cx={12} cy={12} r={3} /></svg>
+                  <span>إعدادات الحساب</span>
+                </div>
+                <button className="logout-btn" onClick={() => removeCookie("token")}>
+                  <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-log-out-icon lucide-log-out"><path d="m16 17 5-5-5-5" /><path d="M21 12H9" /><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" /></svg>
+                  <span>تسجيل الخروج</span>
+                </button>
+              </div>
+            </div>)
+            :
+            <NavLink to="/login" className="btn-delete">
+              <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-log-in-icon lucide-log-in"><path d="m10 17 5-5-5-5" /><path d="M15 12H3" /><path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" /></svg>
+            </NavLink>
+          }
         </div>
+
         <div
           className="menu-toggle"
           onClick={() => setMenuOpen((prev) => !prev)}
@@ -55,6 +84,7 @@ const Header = () => {
             </li>
           ))}
         </ul>
+
         <div className="header-button">
           {cookies?.token?.data?.token && cookies?.token?.data?.token !== "undefined" ?
             (<div className="">
@@ -97,6 +127,3 @@ const Header = () => {
   );
 };
 export default Header;
-
-
-
