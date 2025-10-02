90fee18527c95a6ac8a8fe4c6c17d3aa4e4b38ea
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-10-02 14:45:44 +0200

    add profile-card to .mobile-login

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -67,7 +67,7 @@ const Header = () => {
               </div>
             </div>)
             :
-            <NavLink to="/login" className="btn-delete">
+            <NavLink to="/login" className="mobile_loginBTN">
               <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-log-in-icon lucide-log-in"><path d="m10 17 5-5-5-5" /><path d="M15 12H3" /><path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" /></svg>
             </NavLink>
           }
