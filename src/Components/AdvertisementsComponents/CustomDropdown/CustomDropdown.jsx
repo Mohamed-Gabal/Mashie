c48ee8da8de6806429533b33a9e75ad55e258182
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-10-07 18:34:08 +0200

    Close the menu when you click on out CustomDropdown component

--- src/Components/AdvertisementsComponents/CustomDropdown/CustomDropdown.jsx
+++ src/Components/AdvertisementsComponents/CustomDropdown/CustomDropdown.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import "./CustomDropdown.css";
 
 export default function CustomDropdown({ isOpen, setIsOpen, data, formik, name }) {
     const { setFieldValue } = formik;
+    const dropdownRef = useRef(null);
 
     const handleSelect = (option) => {
         setFieldValue(name, option);
         setIsOpen(false);
-    }
-    
+    };
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setIsOpen(false);
+            }
+        };
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => document.removeEventListener("mousedown", handleClickOutside);
+    }, [setIsOpen]);
+
     return (
-        <div className="dropdown">
-            {/* option */}
+        <div className="dropdown" ref={dropdownRef}>
             {isOpen && (
-                <ul className="dropdown_menu" style={{height: isOpen ? "200px": "0px"}}>
+                <ul className="dropdown_menu" style={{ height: isOpen ? "200px" : "0px" }}>
                     {data.map((item, index) => (
                         <li
                             key={index}
