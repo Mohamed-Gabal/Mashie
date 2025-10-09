c05ab4257db6fbf6daa6864e284e0d9cccc594cc
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-10-09 12:02:18 +0200

    add css style to header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -28,41 +28,16 @@ const Header = () => {
           <input type="search" placeholder="ابحث هنا..." />
         </div>
 
-        <div className="mobile-login">
-          {cookies?.token?.data?.token &&
-            cookies?.token?.data?.token !== "undefined" ? (
-            <div className="">
-              <Link
-                type="button"
-                onClick={() => setToggleProfileCard(!toggleProfileCard)}
-                className="profile_img"
-              >
-                {userData?.image === null ? (
-                  <span className="two_char">
-                    {userData?.name
-                      ?.split(" ")
-                      .map((word) => word[0])
-                      .join("")
-                      .toUpperCase()}
-                  </span>
-                ) : (
-                  <img
-                    src={userData.image}
-                    alt={userData?.name
-                      ?.split(" ")
-                      .map((word) => word[0])
-                      .join("")
-                      .toUpperCase()}
-                    className="user_img"
-                  />
-                )}
-              </Link>
-
-              <div
-                className="profile-card"
-                style={{ height: toggleProfileCard ? "280px" : "0" }}
-              >
-                <div className="user-info">
+        <div className="menu-mobile-toggle">
+          <div className="mobile-login">
+            {cookies?.token?.data?.token &&
+              cookies?.token?.data?.token !== "undefined" ? (
+              <div className="">
+                <Link
+                  type="button"
+                  onClick={() => setToggleProfileCard(!toggleProfileCard)}
+                  className="header_profile_img"
+                >
                   {userData?.image === null ? (
                     <span className="two_char">
                       {userData?.name
@@ -72,92 +47,78 @@ const Header = () => {
                         .toUpperCase()}
                     </span>
                   ) : (
-                    <img
-                      src={userData.image}
-                      alt={userData?.name
-                        ?.split(" ")
-                        .map((word) => word[0])
-                        .join("")
-                        .toUpperCase()}
+                    <img src={userData.image} alt={userData?.name?.split(" ").map((word) => word[0]).join("").toUpperCase()}
                       className="user_img"
                     />
                   )}
-                  <div>
-                    <p className="greeting">أهلا</p>
-                    <p className="username">{userData?.name}</p>
-                  </div>
-                </div>
-                <Link to="/accountUser" className="profile-btn"><button>عرض الملف الشخصي</button></Link>
-                <div className="settings">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={22}
-                    height={22}
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="lucide lucide-settings-icon lucide-settings"
-                  >
-                    <path d="M9.671 4.136a2.34 2.34 0 0 1 4.659 0 2.34 2.34 0 0 0 3.319 1.915 2.34 2.34 0 0 1 2.33 4.033 2.34 2.34 0 0 0 0 3.831 2.34 2.34 0 0 1-2.33 4.033 2.34 2.34 0 0 0-3.319 1.915 2.34 2.34 0 0 1-4.659 0 2.34 2.34 0 0 0-3.32-1.915 2.34 2.34 0 0 1-2.33-4.033 2.34 2.34 0 0 0 0-3.831A2.34 2.34 0 0 1 6.35 6.051a2.34 2.34 0 0 0 3.319-1.915" />
-                    <circle cx={12} cy={12} r={3} />
-                  </svg>
-                  <span>إعدادات الحساب</span>
-                </div>
-                <button
-                  className="logout-btn"
-                  onClick={() => removeCookie("token")}
+                </Link>
+
+                <div
+                  className="profile-card"
+                  style={{ height: toggleProfileCard ? "300px" : "0" }}
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={24}
-                    height={24}
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="lucide lucide-log-out-icon lucide-log-out"
+                  <div className="user-info">
+                    {userData?.image === null ? (
+                      <span className="two_char">
+                        {userData?.name
+                          ?.split(" ")
+                          .map((word) => word[0])
+                          .join("")
+                          .toUpperCase()}
+                      </span>
+                    ) : (
+                      <img
+                        src={userData.image}
+                        alt={userData?.name
+                          ?.split(" ")
+                          .map((word) => word[0])
+                          .join("")
+                          .toUpperCase()}
+                        className="user_img"
+                      />
+                    )}
+                    <div>
+                      <p className="greeting">أهلا</p>
+                      <p className="username">{userData?.name}</p>
+                    </div>
+                  </div>
+                  <Link to="/accountUser" className="show_accountUser"><span>عرض الملف الشخصي</span></Link>
+                  <div className="settings">
+                    <svg xmlns="http://www.w3.org/2000/svg" width={22} height={22} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-settings-icon lucide-settings">
+                      <path d="M9.671 4.136a2.34 2.34 0 0 1 4.659 0 2.34 2.34 0 0 0 3.319 1.915 2.34 2.34 0 0 1 2.33 4.033 2.34 2.34 0 0 0 0 3.831 2.34 2.34 0 0 1-2.33 4.033 2.34 2.34 0 0 0-3.319 1.915 2.34 2.34 0 0 1-4.659 0 2.34 2.34 0 0 0-3.32-1.915 2.34 2.34 0 0 1-2.33-4.033 2.34 2.34 0 0 0 0-3.831A2.34 2.34 0 0 1 6.35 6.051a2.34 2.34 0 0 0 3.319-1.915" />
+                      <circle cx={12} cy={12} r={3} />
+                    </svg>
+                    <span>إعدادات الحساب</span>
+                  </div>
+                  <button
+                    className="logout-btn"
+                    onClick={() => removeCookie("token")}
                   >
-                    <path d="m16 17 5-5-5-5" />
-                    <path d="M21 12H9" />
-                    <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
-                  </svg>
-                  <span>تسجيل الخروج</span>
-                </button>
+                    <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-log-out-icon lucide-log-out">
+                      <path d="m16 17 5-5-5-5" /> <path d="M21 12H9" /> <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
+                    </svg>
+                    <span>تسجيل الخروج</span>
+                  </button>
+                </div>
               </div>
-            </div>
-          ) : (
-            <NavLink to="/login" className="mobile_loginBTN">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width={24}
-                height={24}
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="lucide lucide-log-in-icon lucide-log-in"
-              >
-                <path d="m10 17 5-5-5-5" />
-                <path d="M15 12H3" />
-                <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" />
-              </svg>
-            </NavLink>
-          )}
+            ) : (
+              <NavLink to="/login" className="mobile_loginBTN">
+                <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-log-in-icon lucide-log-in">
+                  <path d="m10 17 5-5-5-5" /><path d="M15 12H3" /><path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" />
+                </svg>
+              </NavLink>
+            )}
+          </div>
+
+          <div
+            className="menu-toggle"
+            onClick={() => setMenuOpen((prev) => !prev)}
+          >
+            {/* <FiAlignJustify /> */}
+            <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-text-align-justify-icon lucide-text-align-justify"><path d="M3 5h18" /><path d="M3 12h18" /><path d="M3 19h18" /></svg>
+          </div>
         </div>
 
-        <div
-          className="menu-toggle"
-          onClick={() => setMenuOpen((prev) => !prev)}
-        >
-          <FiAlignJustify />
-        </div>
         <ul className={`nav ${menuOpen ? "open" : ""}`}>
           {navLinks.map((link, i) => (
             <li key={i}>
@@ -182,20 +143,7 @@ const Header = () => {
                 className="btn_profile"
               >
                 <span>حسابي</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={22}
-                  height={22}
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="lucide lucide-chevron-down-icon lucide-chevron-down"
-                >
-                  <path d="m6 9 6 6 6-6" />
-                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width={22} height={22} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-down-icon lucide-chevron-down"><path d="m6 9 6 6 6-6" /> </svg>
               </Link>
 
               <div
@@ -203,39 +151,17 @@ const Header = () => {
                 style={{ height: toggleProfileCard ? "300px" : "0" }}
               >
                 <div className="user-info">
-                  <img
-                    src={userData.image}
-                    alt={userData?.name
-                      ?.split(" ")
-                      .map((word) => word[0])
-                      .join("")
-                      .toUpperCase()}
-                    className="user_img"
-                  />
+                  <img src={userData.image} alt={userData?.name?.split(" ").map((word) => word[0]).join("").toUpperCase()} className="user_img" />
                   <div>
                     <p className="greeting">أهلا</p>
                     <p className="username">{userData?.name}</p>
                   </div>
                 </div>
-                <Link to="/accountUser" className="profile-btn">
+                <Link to="/accountUser" className="show_accountUser">
                   عرض الملف الشخصي
                 </Link>
                 <div className="settings">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={22}
-                    height={22}
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="lucide lucide-settings-icon lucide-settings"
-                  >
-                    <path d="M9.671 4.136a2.34 2.34 0 0 1 4.659 0 2.34 2.34 0 0 0 3.319 1.915 2.34 2.34 0 0 1 2.33 4.033 2.34 2.34 0 0 0 0 3.831 2.34 2.34 0 0 1-2.33 4.033 2.34 2.34 0 0 0-3.319 1.915 2.34 2.34 0 0 1-4.659 0 2.34 2.34 0 0 0-3.32-1.915 2.34 2.34 0 0 1-2.33-4.033 2.34 2.34 0 0 0 0-3.831A2.34 2.34 0 0 1 6.35 6.051a2.34 2.34 0 0 0 3.319-1.915" />
-                    <circle cx={12} cy={12} r={3} />
-                  </svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" width={22} height={22} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-settings-icon lucide-settings"><path d="M9.671 4.136a2.34 2.34 0 0 1 4.659 0 2.34 2.34 0 0 0 3.319 1.915 2.34 2.34 0 0 1 2.33 4.033 2.34 2.34 0 0 0 0 3.831 2.34 2.34 0 0 1-2.33 4.033 2.34 2.34 0 0 0-3.319 1.915 2.34 2.34 0 0 1-4.659 0 2.34 2.34 0 0 0-3.32-1.915 2.34 2.34 0 0 1-2.33-4.033 2.34 2.34 0 0 0 0-3.831A2.34 2.34 0 0 1 6.35 6.051a2.34 2.34 0 0 0 3.319-1.915" /><circle cx={12} cy={12} r={3} /></svg>
                   <Link to="/settingsUser">إعدادات الحساب</Link>
                 </div>
                 <button
@@ -268,21 +194,7 @@ const Header = () => {
             </NavLink>
           )}
           <NavLink to="/Advertisements" className="btn-add">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width={20}
-              height={20}
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth={2}
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="lucide lucide-plus-icon lucide-plus"
-            >
-              <path d="M5 12h14" />
-              <path d="M12 5v14" />
-            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-plus-icon lucide-plus"><path d="M5 12h14" /><path d="M12 5v14" /> </svg>
             <span>اضف عرضك</span>
           </NavLink>
         </div>
