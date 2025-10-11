0e7e7e3fad099515711aee5cd5d51277b2faf4e1
Mohamed Gabal <abogabal672@gmail.com>
2025-10-11 15:38:23 +0300

    api

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -13,16 +13,28 @@ import { IoNotificationsOutline } from "react-icons/io5";
 import { TiMessage } from "react-icons/ti";
 import { MdFavoriteBorder } from "react-icons/md";
 // استدعاء للتنقب لين لاصفحات
-// import { useNavigate } from "react-router-dom";
-// import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 const Footer = () => {
   // لتخزين التوكين
-  // const [cookie] = useCookies(["token"]);
+  const cookie = useCookies(["token"]);
 
   // للتنقل بين الصفحات
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
+  // handle Logout for Icons
+  const handleCheckLogout = (path) => {
+    // تاكيد التوكين
+    const token = cookie?.token?.data?.token;
+
+    if (token) {
+      // تشيك
+      navigate(path);
+    } else {
+      navigate("/login");
+    }
+  };
 
   return (
     <footer className="footer">
@@ -112,22 +124,28 @@ const Footer = () => {
           <MdOutlineHome />
           <span>الرئيسيه</span>
         </Link>
-        <Link to="/favoritesUser">
-          <MdFavoriteBorder />
-          <span>المفضله</span>
-        </Link>
+        <div onClick={() => handleCheckLogout("/favoritesUser")}>
+          <Link>
+            <MdFavoriteBorder />
+            <span>المفضله</span>
+          </Link>
+        </div>
         <Link to="/Advertisements">
           <IoMdAdd className="active-icon" />
           <span>اضافه عرض</span>
         </Link>
-        <Link to='/notifactionsUser'>
-          <IoNotificationsOutline />
-          <span>الاشعارات</span>
-        </Link>
-        <Link>
-          <TiMessage />
-          <span>الرسائل</span>
-        </Link>
+        <div onClick={() => handleCheckLogout("/notifactionsUser")}>
+          <Link>
+            <IoNotificationsOutline />
+            <span>الاشعارات</span>
+          </Link>
+        </div>
+        <div>
+          <Link>
+            <TiMessage />
+            <span>الرسائل</span>
+          </Link>
+        </div>
       </div>
     </footer>
   );
