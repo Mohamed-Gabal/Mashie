2808e3efe5f8ff6cf1907efa1a54c950e2432bee
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-10-20 14:47:08 +0200

    add ShowAnyUser page

--- src/Pages/ShowAnyUser/ShowAnyUser.jsx
+++ src/Pages/ShowAnyUser/ShowAnyUser.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import "./showAnyUserStyle.css"
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { CiLocationOn, CiStopwatch } from 'react-icons/ci';
+import { timeSince } from '../SpecificCategory/SpecificCategory';
 
 export default function ShowAnyUser() {
     const { userID } = useParams();
@@ -46,12 +48,12 @@ export default function ShowAnyUser() {
                             <div className="user_img_container">
                                 <img src={userData?.user?.profile_image} alt="img" loading="lazy" />
                             </div>
-                            <div className="status_dot" style={{backgroundColor:userData?.user?.is_online ? "var(--main-color)" : "var(--parg-color)" }}/>
+                            <div className="status_dot" style={{ backgroundColor: userData?.user?.is_online ? "var(--main-color)" : "var(--parg-color)" }} />
                         </div>
                     </div>
 
                     <div className="user_info">
-                        <h3 className="user_name">{userData?.user?.name}</h3>
+                        <h3 className="user_name">{userData?.user?.name?.split(" ").slice(0, 2).join(" ")}</h3>
                         <h6 className="user_status">
                             <span>آخر ظهور  </span>
                             <span>{userData?.user?.last_seen_at}</span>
@@ -59,6 +61,51 @@ export default function ShowAnyUser() {
                     </div>
 
                     {errorMessage && <p className="error_message">{errorMessage}</p>}
+                </div>
+
+                <div className="categories_items">
+                    {userData?.ads?.map((cat) => (
+                        <div
+                            key={cat.id_ads}
+                            className={`category_card`}
+                        >
+                            <div className="card_image">
+                                <img
+                                    src={cat.images?.[0] ? `https://api.mashy.sand.alrmoz.com/storage/${cat.images[0]}` : "/placeholder.png"}
+                                    alt={cat?.information?.title}
+                                />
+
+                            </div>
+
+                            <div className="card_user">
+                                {cat.user?.profile_image ? (
+                                    <img src={cat.user.profile_image} alt={cat.seller?.name} />
+                                ) : (
+                                    <div className="avatar_placeholder">
+                                        {cat?.seller?.name?.split(" ").map(word => word[0]).join("").toUpperCase()}
+                                    </div>
+                                )}
+                                <span>{cat.seller?.name}</span>
+                            </div>
+
+                            <div className="card_body">
+                                <h3>{cat?.information?.title.substring(0, 18)}...</h3>
+                                <div className="card_meta">
+                                    <div className="ciLocationOn">
+                                        <CiLocationOn style={{ color: "var(--main-color)", fontSize: "12px", fontWeight: "bold" }} />
+                                        <span>{cat?.user?.area || "غير محدد"}</span>
+                                    </div>
+                                    <div className="ciStopwatch">
+                                        <CiStopwatch style={{ color: "var(--main-color)", fontSize: "12px", fontWeight: "bold" }} />
+                                        <span>{timeSince(cat.created_at)}</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <Link to={`/${cat.id_ads}`} className="details_link">
+                                عرض التفاصيل
+                            </Link>
+                        </div>
+                    ))}
                 </div>
             </div>
         </section>
