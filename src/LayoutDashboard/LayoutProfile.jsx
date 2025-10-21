71067c4e47de3b495d907465f7191287d8227fa4
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-10-21 11:40:20 +0200

    use React.lazy and Path protection

--- src/LayoutDashboard/LayoutProfile.jsx
+++ src/LayoutDashboard/LayoutProfile.jsx
@@ -3,8 +3,13 @@ import "./layoutProfile.css";
 import HeaderDashboard from "../Components/HeaderDashboard/HeaderDashboard";
 import SidebarDashboard from "../Components/SidebarDashboard/SidebarDashboard";
 import { useLocation } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import LoginRequiredCard from "../Components/AdvertisementsComponents/LoginRequiredCard/LoginRequiredCard";
 
 const LayoutProfile = ({ children }) => {
+  const [cookies] = useCookies(["token"]);
+  const token = cookies?.token?.data?.token ?? null;
+
   // لو الناف بار موجود داخل الرسايل يخفيه
   const location = useLocation();
 
@@ -19,16 +24,16 @@ const LayoutProfile = ({ children }) => {
 
     const handleClickOutside = (e) => {
       const target = e.target;
-    
+
       if (
         (sidebarRef.current && sidebarRef.current.contains(target)) ||
         (toggleBtnRef.current && toggleBtnRef.current.contains(target))
       ) {
         return;
       }
-    
+
       setToggleSidebar(false);
-    };    
+    };
 
     document.addEventListener("mousedown", handleClickOutside);
 
@@ -77,32 +82,35 @@ const LayoutProfile = ({ children }) => {
   };
   return (
     <div className="layoutProfile">
-      <div className="layoutProfile_main">
+      {token ?
+        <div className="layoutProfile_main">
+          <div className="toggle_sidebar" onClick={() => setToggleSidebar(!toggleSidebar)} ref={toggleBtnRef} >
+            {toggleSidebar ?
+              <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg>
+              :
+              <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-columns2-icon lucide-columns-2"><rect width={18} height={18} x={3} y={3} rx={2} /><path d="M12 3v18" /></svg>
+            }
+          </div>
+          {/* السايدبار الثابت */}
+          <div className="layoutProfile_sidebar">
+            <SidebarDashboard
+              toggleSidebar={toggleSidebar}
+              setToggleSidebar={setToggleSidebar}
+              sidebarRef={sidebarRef}
+            />
+          </div>
 
-        <div className="toggle_sidebar" onClick={() => setToggleSidebar(!toggleSidebar)} ref={toggleBtnRef} >
-          {toggleSidebar ?
-            <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg>
-            :
-            <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-columns2-icon lucide-columns-2"><rect width={18} height={18} x={3} y={3} rx={2} /><path d="M12 3v18" /></svg>
-          }
+          {/* باقي الصفحة (الهيدر + المحتوى) */}
+          <div className="layoutProfile_body">
+            {!navNone.includes(location.pathname) && (
+              <HeaderDashboard title={cur.title} desc={cur.desc} />
+            )}
+            <div className="layoutProfile_content">{children}</div>
+          </div>
         </div>
-        {/* السايدبار الثابت */}
-        <div className="layoutProfile_sidebar">
-          <SidebarDashboard
-            toggleSidebar={toggleSidebar}
-            setToggleSidebar={setToggleSidebar}
-            sidebarRef={sidebarRef}
-          />
-        </div>
-
-        {/* باقي الصفحة (الهيدر + المحتوى) */}
-        <div className="layoutProfile_body">
-          {!navNone.includes(location.pathname) && (
-            <HeaderDashboard title={cur.title} desc={cur.desc} />
-          )}
-          <div className="layoutProfile_content">{children}</div>
-        </div>
-      </div>
+        :
+        <LoginRequiredCard />
+      }
     </div>
   );
 };
