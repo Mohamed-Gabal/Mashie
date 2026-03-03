9a5ae2cc2d3394cdd54856c7a5243a38add966d9
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-03-03 17:45:10 +0200

    add categories icon to custom laibrary icon

--- src/Components/UI/Icons/Icons.jsx
+++ src/Components/UI/Icons/Icons.jsx
@@ -93,3 +93,85 @@ export const PlusIcon = ({ className = "", width = 22, height = 22 }) => (
         </svg>
     </div>
 );
+
+// ---------------- categories icon ----------------
+// ---------------- categories icon ----------------
+export const RealEstateIcon = ({ className = "", width = 22, height = 22 }) => (
+    <div className={className}>
+        <svg width={width} height={height} viewBox={`0 0 ${width} ${height}`} fill="none" stroke="currentColor" aria-hidden="true">
+            <g clipPath="url(#clip0_4020_9502)">
+                <path d="M10.625 16.875V2.49999C10.6249 2.38688 10.5942 2.27591 10.536 2.1789C10.4778 2.0819 10.3944 2.0025 10.2946 1.94918C10.1949 1.89585 10.0825 1.8706 9.96956 1.87611C9.85658 1.88162 9.74722 1.91768 9.65313 1.98046L3.40313 6.14687C3.3174 6.20406 3.24715 6.28157 3.19862 6.37248C3.1501 6.4634 3.12481 6.56491 3.125 6.66796V16.875" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M10.625 6.875H16.25C16.4158 6.875 16.5747 6.94085 16.6919 7.05806C16.8092 7.17527 16.875 7.33424 16.875 7.5V16.875" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M1.25 16.875H18.75" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M8.125 8.75V10" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M5.625 8.75V10" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M5.625 13.125V14.375" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M8.125 13.125V14.375" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+            </g>
+            <defs>
+                <clipPath id="clip0_4020_9502">
+                    <rect width={width} height={height} fill="white" />
+                </clipPath>
+            </defs>
+        </svg>
+    </div>
+);
+
+export const DevicesIcon = ({ className = "", width = 22, height = 22 }) => (
+    <div className={className}>
+        <svg width={width} height={height} viewBox={`0 0 ${width} ${height}`} fill="none" stroke="currentColor" aria-hidden="true">
+            <g clipPath="url(#clip0_4021_10475)">
+                <path d="M11.25 14.375H2.5C2.16848 14.375 1.85054 14.2433 1.61612 14.0089C1.3817 13.7745 1.25 13.4565 1.25 13.125V7.5C1.25 7.16848 1.3817 6.85054 1.61612 6.61612C1.85054 6.3817 2.16848 6.25 2.5 6.25H11.25" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M8.75 16.875H5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M16.25 5.625H13.75" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M16.25 8.125H13.75" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M18.125 3.125H11.875C11.5298 3.125 11.25 3.40482 11.25 3.75V16.25C11.25 16.5952 11.5298 16.875 11.875 16.875H18.125C18.4702 16.875 18.75 16.5952 18.75 16.25V3.75C18.75 3.40482 18.4702 3.125 18.125 3.125Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M6.875 14.375V16.875" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M15 15C15.5178 15 15.9375 14.5803 15.9375 14.0625C15.9375 13.5447 15.5178 13.125 15 13.125C14.4822 13.125 14.0625 13.5447 14.0625 14.0625C14.0625 14.5803 14.4822 15 15 15Z" fill="#1ABC9C" />
+            </g>
+            <defs>
+                <clipPath id="clip0_4021_10475">
+                    <rect width={width} height={height} fill="white" />
+                </clipPath>
+            </defs>
+        </svg>
+    </div>
+);
+
+export const CarsIcon = ({ className = "", width = 22, height = 22 }) => (
+    <div className={className}>
+        <svg width={width} height={height} viewBox={`0 0 ${width} ${height}`} fill="none" stroke="currentColor" aria-hidden="true">
+            <g clipPath="url(#clip0_4020_9493)">
+                <path d="M1.25 8.75H18.75" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M17.5 13.75V15.625C17.5 15.7908 17.4342 15.9497 17.3169 16.0669C17.1997 16.1842 17.0408 16.25 16.875 16.25H15C14.8342 16.25 14.6753 16.1842 14.5581 16.0669C14.4408 15.9497 14.375 15.7908 14.375 15.625V13.75" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M5.625 13.75V15.625C5.625 15.7908 5.55915 15.9497 5.44194 16.0669C5.32473 16.1842 5.16576 16.25 5 16.25H3.125C2.95924 16.25 2.80027 16.1842 2.68306 16.0669C2.56585 15.9497 2.5 15.7908 2.5 15.625V13.75" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M5 11.25H6.25" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M13.75 11.25H15" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M17.5 8.75L15.1648 3.49609C15.1157 3.38564 15.0356 3.29179 14.9343 3.22592C14.8329 3.16006 14.7146 3.125 14.5938 3.125H5.40625C5.28537 3.125 5.16708 3.16006 5.06572 3.22592C4.96435 3.29179 4.88426 3.38564 4.83516 3.49609L2.5 8.75V13.75H17.5V8.75Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+            </g>
+            <defs>
+                <clipPath id="clip0_4020_9493">
+                    <rect width={20} height={20} fill="white" />
+                </clipPath>
+            </defs>
+        </svg>
+    </div>
+);
+
+export const JobsIcon = ({ className = "", width = 22, height = 22 }) => (
+    <div className={className}>
+        <svg width={width} height={height} viewBox={`0 0 ${width} ${height}`} fill="none" stroke="currentColor" aria-hidden="true">
+            <g clipPath="url(#clip0_4021_10485)">
+                <path d="M16.363 4.00156L6.187 2.20359C5.84709 2.14354 5.52285 2.3704 5.46279 2.71032L3.13034 15.9113C3.07029 16.2512 3.29715 16.5755 3.63707 16.6355L13.813 18.4335C14.153 18.4935 14.4772 18.2667 14.5373 17.9268L16.8697 4.72577C16.9298 4.38585 16.7029 4.06161 16.363 4.00156Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M7.59534 4.98999L14.0781 6.1353" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M13.6453 8.59705L7.16174 7.45251" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M9.97029 10.487L6.72888 9.91431" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+            </g>
+            <defs>
+                <clipPath id="clip0_4021_10485">
+                    <rect width={20} height={20} fill="white" transform="translate(0 0.317383)" />
+                </clipPath>
+            </defs>
+        </svg>
+    </div>
+);
