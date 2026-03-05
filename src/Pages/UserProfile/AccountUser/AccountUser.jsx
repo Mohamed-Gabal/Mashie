14b21e38bf908dd0a4977d5954cd490ccc268f2e
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-03-05 14:15:01 +0200

    add AccountUser page

--- src/Pages/UserProfile/AccountUser/AccountUser.jsx
+++ src/Pages/UserProfile/AccountUser/AccountUser.jsx
@@ -13,7 +13,7 @@ export default function AccountUser() {
         }
     }, [userID, token]);
     return (
-        <section className='accountUser_container'>
+        <section className='userprofile-page'>
             <header className='userProfile_header'>
                 <h2>ملفك الشخصي من <span>ماشي</span></h2>
                 <p>عرّف بنفسك واجعل ملفك أكثر وضوحًا</p>
