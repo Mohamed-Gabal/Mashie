2b63c599fd20a44bb0368182f296ec8f701eb923
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-03-05 14:26:14 +0200

    add userNotifaction css style

--- src/Pages/UserProfile/UserNotifactions/UserNotifactions.jsx
+++ src/Pages/UserProfile/UserNotifactions/UserNotifactions.jsx
@@ -3,7 +3,7 @@ import "./UserNotifactionsStyle.css"
 
 export default function UserNotifactions() {
     return (
-        <section className='UserNotifactions_container'>
+        <section className='userprofile-page'>
             <header className='userProfile_header'>
                 <h2>إشعاراتك</h2>
                 <p>تابع أحدث المستجدات حول عروضك ورسائلك</p>
